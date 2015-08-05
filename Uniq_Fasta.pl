6ce5092b39e3e85c9af9ccc4cd42163534674c21
Minesh Jethva <minesh.1291@gmail.com>
2015-08-05 12:22:37 +0530

    Create Uniq_Fasta.pl

---
+++ Uniq_Fasta.pl
@@ -0,0 +1,23 @@
+##
+#	Required : preinstalled BioPerl (Bio::SeqIO)
+#	Usage : perl Uniq_Fasta.pl <InputFile> > <OutputFile>
+##
+
+use strict;
+use warnings;
+use Bio::SeqIO;
+
+my ( $file, %hash, %seen ) = shift;
+
+for my $i ( 0 .. 1 ) {
+    my $in = Bio::SeqIO->new( -file   => $file, -format => 'Fasta' );
+
+    while ( my $seq = $in->next_seq() ) {
+        if ( !$i ) {
+            $hash{ $seq->id } = $seq->desc if !defined $hash{ $seq->id } or length $seq->desc > length $hash{ $seq->id };
+        }
+        else {
+            print '>'. $seq->id . ' ' . $hash{ $seq->id } . "\n" . $seq->seq . "\n" if !$seen{ $seq->id }++;
+        }
+    }
+}
