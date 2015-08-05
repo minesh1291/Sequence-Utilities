800ea2e6950cf8e02104469d0f39d93e9e67d587
Minesh Jethva <minesh.1291@gmail.com>
2015-08-05 12:34:07 +0530

    Create Rev_Comp.py

---
+++ Rev_Comp.py
@@ -0,0 +1,16 @@
+##
+#	Use : To make reverse compliment of multiple fasta 
+#	Require : preinstalled BioPython
+#	Usage : python Rev_Comp.py <InputFile> > <OutputFile>
+##
+
+import sys
+
+file=sys.argv[1]
+
+print >> sys.stderr, file 
+
+from Bio import SeqIO
+for record in SeqIO.parse(file, "fasta"):
+    print ">"+record.id
+    print record.seq.reverse_complement()
